cecf1fe2c6ad939d2e23d027856874be91228783
Nikav7 <112890224+Nikav7@users.noreply.github.com>
2025-02-06 18:04:11 +0100

    Update SpeechSynthesisGrad_TTS.py
    multi-speaker added

--- SpeechSynthesisGrad_TTS.py
+++ SpeechSynthesisGrad_TTS.py
@@ -30,7 +30,7 @@ GRAD_TTS_CKPT_LIBRI = os.path.join(BASE_PATH, "SpeechBackbones/GradTTS/checkpts/
 HIFIGAN_CKPT = os.path.join(BASE_PATH, "SpeechBackbones/GradTTS/checkpts/hifigan.pt")
 HIFIGAN_CONFIG = os.path.join(BASE_PATH, "SpeechBackbones/GradTTS/checkpts/hifigan-config.json")
 CMU_DICT = os.path.join(BASE_PATH, "SpeechBackbones/GradTTS/resources/ita_dictionary")
-N_SPKS = 1  # 247 for Libri-TTS model and 1 for single speaker (LJSpeech)
+N_SPKS = 1  # 2 for multi_AB_LJ, 247 for Libri-TTS model and 1 for single speaker
 
 # Initialize Grad-TTS model
 def initialize_grad_tts():
@@ -89,16 +89,23 @@ def reconstruct_waveform(y_dec, hifigan):
     sample_rate = 22050
     wavfile.write('generated_audio.wav', sample_rate, audio_data)
 
-def text_to_speech(text = "Random text wow ok."):
+def text_to_speech(text = "Random text wow ok.", speaker_id = None):
     generator = initialize_grad_tts()
     hifigan = initialize_hifigan()
     x, x_lengths = preprocess_text(text)
 
+    if not isinstance(speaker_id, type(None)):
+        assert params.n_spks > 1, "Ensure you set right number of speakers in `params.py`."
+        spk = torch.LongTensor([speaker_id])
+    else:
+        spk = None
+
     t = dt.datetime.now()
     y_enc, y_dec, attn = generator.forward(x, x_lengths, n_timesteps=100, temperature=1.3,
-                                       stoc=False, spk=None if N_SPKS==1 else torch.LongTensor([15]).cuda(),
-                                       length_scale=1.05)
+                                       stoc=False, spk=spk, #spk=None if N_SPKS==1 else torch.LongTensor([15]),
+                                       length_scale=1.0)
     t = (dt.datetime.now() - t).total_seconds()
+    #RTF (Real-Time Factor) is a measure of how fast an audio generation or processing system runs compared to real-time playback.
     print(f'Grad-TTS RTF: {t * 22050 / (y_dec.shape[-1] * 256)}')
 
     plot_results(y_enc, y_dec, attn)
@@ -106,13 +113,38 @@ def text_to_speech(text = "Random text wow ok."):
 
 
 
-# Example usage
 if __name__ == "__main__":
     print(f"Using GPU: {torch.cuda.is_available()}")  # Check if GPU is available
+    
+    # ITA
+    #sent_1 = "La società dell'impero ottomano non conosce la nobiltà di nascita, è una società dove non esiste nemmeno il concetto di nobiltà, il pregiudizio per cui solo chi ha un grande nome e una famiglia prestigiosa alle spalle ha diritto ad occupare i posti di comando."
+    #sent_2 = "L'Europa si va dividendo in regni su base geografica e in una certa misura anche nazionale."
+    #sent_3 = "Tutti i leader dell’impero ottomano erano nati cristiani ed erano figli di povera gente."
+    #sent_4 = ""Ti prego di ascoltare questa voce convertita con i dati provenienti da un video del prof. Alessandro Barbero e il dataset LJ Speech. Steve Jobs era il San Francesco del medioevo."
+    #sent_5 = "Per gli ordini religiosi del medioevo la riconoscibilità è un elemento fondamentale del successo, perché sono in concorrenza fra loro."    
+    #sent_6 = "L’idea che i pascià, i visir e gli ammiragli, nei loro sontuosi palazzi di Costantinopoli, da dove governano un impero, siano tutti figli di pastori sconvolge gli osservatori europei."
+    #sent_7 = "Nella società dell’impero ottomano non c’è niente che temperi la volontà assoluta del sultano, non ci sono forze organizzate o corpi, come si diceva nell’Occidente moderno: non c’è una Chiesa, non c’è l’università, non i comuni urbani, e non c’è una nobiltà. Il risultato è una società al tempo stesso più aperta al talento e più esposta alla tirannia."
+    
+    # ENG
+    #sent1 = "The Ottoman Empire society does not know the nobility of birth, it is a society where there is not even the concept of nobility, the prejudice that only those who have a great name and a prestigious family behind them have the right to occupy the positions of power."
+    #sent2 = "The quick brown fox jumps over the lazy dog?"
+    #sent3 = "All the leaders of the Ottoman Empire were born Christians and were the children of poor people."
+    #sent4 = "The idea that the pashas, viziers, and admirals, which are in their sumptuous palaces in Constantinople ruling an empire, are all shepherds’ sons, shocks the European observers."
+    #sent5 = "Please listen to this converted voice, using data from prof. Alessandro Barbero and the LJ Speech dataset. Steve Jobs was the Saint Francis of the Middle Ages."
+    #sent6 = "For medieval religious orders, recognizability is a fundamental element of success because they compete with each other."
+    #sent7 = "In Ottoman imperial society, nothing moderates the absolute will of the sultan; there are no organized forces or bodies, as they were called in the modern West: there is no Church, no university, no urban communes, and no nobility. The result is a society that is at once more open to talent and more vulnerable to tyranny."
 
-    #text = "questa è la voce che risulta dal finetuning di GradTTS con la voce di Alessandro Barbero, che ne pensi?
-    text = "Cara Francesca, come stai? Ti scrivo questa lettera per dirti che, settimana prossima verrò a trovarti! La scuola è finita e ho superato gli esami con ottimi voti! L'estate è finalmente arrivata e non vedo l'ora di poter trascorrere delle giornate in spiaggia insieme a te, Lucia e Stefano. Penso spesso a tutte le cose che potremmo fare: andare allo zoo, fare shopping, mangiare gelati, fare lunghe passeggiate e, ovviamente, andare al mare! In realtà ti scrivo per chiederti una cosa: posso portare con me Billy? E' il mio gatto ed è molto dolce. Ti piacerà sicuramente! Domani vado a Roma per una gita. Sono molto emozionata! Ho sempre voluto vedere il Colosseo, Piazza San Pietro.  Mi piacerebbe tu fossi qui con me! Ora vado ad aiutare la mamma con la cena. "
-    #text = "Recently, denoising diffusion probabilistic models and generative score matching have shown high potential in modelling complex data distributions while stochastic calculus has provided a unified point of view on these techniques allowing for flexible inference schemes. In this paper we introduce Grad-TTS, a novel text-to-speech model with score-based decoder producing mel-spectrograms by gradually transforming noise predicted by encoder and aligned with text input by means of Monotonic Alignment Search. The framework of stochastic differential equations helps us to generalize conventional diffusion probabilistic models to the case of reconstructing data from noise with different parameters and allows to make this reconstruction flexible by explicitly controlling trade-off between sound quality and inference speed. Subjective human evaluation shows that Grad-TTS is competitive with state-of-the-art text-to-speech approaches in terms of Mean Opinion Score."
-    #"Here are the match lineups for the Colombia Haiti match."
-    audio = text_to_speech(text)
-    #print(f"Audio saved to output.wav")
+    # REAL AUDIOS
+    # ita from AB dataset
+    #text = "Per gli ordini religiosi del medioevo la riconoscibilità è un elemento fondamentale del successo, perché sono in concorrenza fra loro."
+
+    # eng from LJSpeech dataset
+    text = "Under these circumstances, unnatural as they are, with proper management, the bean will thrust forth its radicle and its plumule."
+
+    # Long descriptive sentences
+    #text = "Recentemente, i modelli probabilistici di diffusione e la qualità dell'output generato hanno mostrato un alto potenziale nella modellazione di distribuzioni di dati complesse, mentre il calcolo stocastico ha fornito un punto di vista unificato su queste tecniche, consentendo schemi di inferenza flessibili. Questo audio è generato con Grad-TiTieS, un nuovo modello di sintesi vocale con decodificatore che produce spettrogrammi mel trasformando gradualmente il rumore previsto dal codificatore e allineato con l'input di testo, mediante ricerca di allineamento monotonico. Il quadro delle equazioni differenziali stocastiche ci aiuta a generalizzare i modelli probabilistici di diffusione convenzionali per ricostruire dati dal rumore, con parametri diversi. Ciò consente di rendere flessibile questa ricostruzione controllando esplicitamente il compromesso tra qualità del suono e velocità di inferenza. La valutazione umana soggettiva mostra che Grad-TTS è competitivo con gli approcci di sintesi vocale all'avanguardia in termini di Mean Opinion Score."
+    #text = "Recently, denoising diffusion probabilistic models and generative score matching have shown high potential in modelling complex data distributions while stochastic calculus has provided a unified point of view on these techniques allowing for flexible inference schemes. This audio is generated with Grad-TTS, a novel text-to-speech model with score-based decoder producing mel-spectrograms by gradually transforming noise predicted by encoder and aligned with text input by means of Monotonic Alignment Search. The framework of stochastic differential equations helps us to generalize conventional diffusion probabilistic models to the case of reconstructing data from noise with different parameters and allows to make this reconstruction flexible by explicitly controlling trade-off between sound quality and inference speed. Subjective human evaluation shows that Grad-TTS is competitive with state-of-the-art text-to-speech approaches in terms of Mean Opinion Score."
+
+    
+    audio = text_to_speech(text, speaker_id=None)
+    #print(f"Audio saved to generated_audio.wav")
